702e2585bd26ce75399f9218b6da81a1b0697ffb
SadatArefin <sadat.arefin.rafat@gmail.com>
2023-03-02 18:09:35 +0600

    Conditional odd or even loop

--- GeneralProgrammingProblems/src/Main.java
+++ GeneralProgrammingProblems/src/Main.java
@@ -2,10 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        executeTryLoop();
+    }
+
+    public static void executeTryLoop() {
         Scanner inputArrayLength = new Scanner(System.in);
+        Scanner inputSumCondition = new Scanner(System.in);
+        System.out.println("How many numbers?");
         int arrayLength= inputArrayLength.nextInt();
         TryLoop loop=new TryLoop(arrayLength);
         loop.forLoopScan();
         loop.forLoopPrint();
+        loop.forLoopSum();
+        System.out.println("Sum of even or odd numbers?");
+        String sumCondition = inputSumCondition.nextLine();
+        loop.forLoopConditionalSum(sumCondition);
     }
 }
--- GeneralProgrammingProblems/src/TryLoop.java
+++ GeneralProgrammingProblems/src/TryLoop.java
@@ -4,7 +4,7 @@ public class TryLoop {
     private int start;
     private int end;
     private int increment;
-    private int[] arrInt = new int[10000];
+    private final int[] arrInt = new int[10000];
     public void setStart(int start) {
         this.start = start;
     }
@@ -33,14 +33,42 @@ public class TryLoop {
         setIncrement(1);
     }
     public void forLoopPrint(){
+        System.out.println("output:");
         for (int i = getStart(); i < getEnd(); i=i+getIncrement()) {
             System.out.println(arrInt[i]);
         }
     }
     public void forLoopScan(){
         Scanner inputInt = new Scanner(System.in);
+        System.out.println("Give Input:");
         for (int i = getStart(); i < getEnd(); i=i+getIncrement()) {
             arrInt[i]=inputInt.nextInt();
         }
+    }
+    public void forLoopSum(){
+        int sum=0;
+        Scanner inputInt = new Scanner(System.in);
+        for (int i = getStart(); i < getEnd(); i=i+getIncrement()) {
+            sum+=arrInt[i];
+        }
+        System.out.println("Sum is: "+sum);
+    }
+    public void forLoopConditionalSum(String parameter){
+        int sum=0;
+        Scanner inputInt = new Scanner(System.in);
+        for (int i = getStart(); i < getEnd(); i=i+getIncrement()) {
+            if (parameter.equalsIgnoreCase("odd")){
+                if(arrInt[i]%2!=0){
+                    sum+=arrInt[i];
+                }
+            }
+            else if (parameter.equalsIgnoreCase("even")){
+                if(arrInt[i]%2==0){
+                    sum+=arrInt[i];
+                }
+            }
+
+        }
+        System.out.println("Sum is: "+sum);
     }
 }
